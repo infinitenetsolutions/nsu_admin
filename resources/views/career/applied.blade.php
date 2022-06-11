ca2ddc6ba58fea8326bd653b067d9f5fd3d83134
raja <raja@raja.com>
2022-06-11 17:17:36 +0530

    all done

--- resources/views/career/applied.blade.php
+++ resources/views/career/applied.blade.php
@@ -111,7 +111,7 @@
                                                     <td> {{ $career->phone ?? '' }} </td>
                                                     
                                                         <?php   $career_name = DB::table('career_tbl')->find($career->career_id); ?>
-                                                  <td>{{ " - ".$career_name->sub_title }} </td>
+                                                  <td>{{ $career_name->title ." - ".$career_name->sub_title }} </td>
 
 
                                                     <td> <a href="https://nsuniv.ac.in/nsularavel1/public/upload/career/{{ $career->resume ?? '' }}"
