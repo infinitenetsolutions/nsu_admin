a294e8c0847507054bc6469460159bf620682e98
raja <raja@raja.com>
2022-06-16 17:00:38 +0530

    all den

--- resources/views/contactus/index.blade.php
+++ resources/views/contactus/index.blade.php
@@ -1,54 +1,54 @@
 <x-layout>
     @slot('title', 'Contact Us')
-        @slot('body')
+    @slot('body')
 
 
-            <!-- Page Wrapper -->
-            <div id="wrapper">
+        <!-- Page Wrapper -->
+        <div id="wrapper">
 
-                @include('include.aside')
-                <!-- Content Wrapper -->
-                <div id="content-wrapper" class="d-flex flex-column">
+            @include('include.aside')
+            <!-- Content Wrapper -->
+            <div id="content-wrapper" class="d-flex flex-column">
 
-                    <!-- Main Content -->
-                    <div id="content">
+                <!-- Main Content -->
+                <div id="content">
 
-                        <!-- Topbar -->
-                        @include('include.navbar')
-                        <!-- End of Topbar -->
-                        {{-- adding the model --}}
-                        {{-- @include('Contact Us.insert') --}}
-                        <!-- Begin Page Content -->
-                        <div class="container-fluid">
+                    <!-- Topbar -->
+                    @include('include.navbar')
+                    <!-- End of Topbar -->
+                    {{-- adding the model --}}
+                    {{-- @include('Contact Us.insert') --}}
+                    <!-- Begin Page Content -->
+                    <div class="container-fluid">
 
-                            <!-- Page Heading -->
-                            <section class="content-header">
-                                <div class="container-fluid">
-                                    <div class="row mb-2">
-                                        <div class="col-sm-6">
-                                            <h2>Contact Us</h2>
-                                        </div>
-                                        <div class="col-sm-6">
-                                            <ol class="breadcrumb float-sm-right">
-                                                <li class="breadcrumb-item"><a href="/">Home</a></li>
-                                                <li class="breadcrumb-item active">Contact Us</li>
-                                            </ol>
-                                        </div>
+                        <!-- Page Heading -->
+                        <section class="content-header">
+                            <div class="container-fluid">
+                                <div class="row mb-2">
+                                    <div class="col-sm-6">
+                                        <h2>Contact Us</h2>
                                     </div>
-                                </div><!-- /.container-fluid -->
-                            </section>
-                            <!-- DataTales Example -->
-                            <div class="card shadow mb-4">
-                                <div class="card-header py-3 row">
-                                    <div class="col-6">
-                                        <h6 class="m-0 font-weight-bold text-primary">Contact Us </h6>
+                                    <div class="col-sm-6">
+                                        <ol class="breadcrumb float-sm-right">
+                                            <li class="breadcrumb-item"><a href="/">Home</a></li>
+                                            <li class="breadcrumb-item active">Contact Us</li>
+                                        </ol>
                                     </div>
-                                    {{-- <div class="col-6"> <button type="button"
+                                </div>
+                            </div><!-- /.container-fluid -->
+                        </section>
+                        <!-- DataTales Example -->
+                        <div class="card shadow mb-4">
+                            <div class="card-header py-3 row">
+                                <div class="col-6">
+                                    <h6 class="m-0 font-weight-bold text-primary">Contact Us </h6>
+                                </div>
+                                {{-- <div class="col-6"> <button type="button"
                                             class="btn btn-primary btn-sm float-right " data-toggle="modal"
                                             data-target=".bd-example-modal-lg">Add</button>
                                     </div> --}}
-                                </div>
-                                @if (session('store'))
+                            </div>
+                            @if (session('store'))
                                 <div class="alert alert-success">
                                     {{ session('store') }}
                                 </div>
@@ -73,82 +73,79 @@
                                     {{ session('status1') }}
                                 </div>
                             @endif
-                                <div class="card-body">
-                                    <div class="table-responsive">
-                                        <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
-                                            <thead class="text-dark">
+                            <div class="card-body">
+                                <div class="table-responsive">
+                                    <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                                        <thead class="text-dark">
+                                            <tr>
+                                            <tr>
+                                                <th>S.NO</th>
+                                                <th>Name </th>
+                                                <th>Phone </th>
+                                                <th>Email</th>
+                                                <th>Subject</th>
+                                                <th>Date</th>
+                                                <th>Massage</th>
+                                                <th>Delete </th>
+                                            </tr>
+                                            </tr>
+                                        </thead>
+                                        <tfoot class="text-dark">
+                                            <tr>
+                                                <th>S.NO</th>
+                                                <th>Name </th>
+                                                <th>Phone </th>
+                                                <th>Email</th>
+                                                <th>Subject</th>
+                                                <th>Date</th>
+                                                <th>Massage</th>
+                                                <th>Delete </th>
+                                            </tr>
+                                        </tfoot>
+                                        <tbody>
+                                            @foreach ($data as $slider)
                                                 <tr>
-                                                <tr>
-                                                    <th>S.NO</th>
-                                                    <th>Name </th>
-                                                    <th>Phone </th>
-                                                    <th>Email</th>
-                                                    <th>Subject</th>
-                                                    <th>Massage</th>
-                                                    <th>Delete </th>
+                                                    <td>{{ $loop->iteration }}</td>
+                                                    <td> {{ $slider->name ?? '' }} </td>
+                                                    <td> {{ $slider->email ?? '' }} </td>
+                                                    <td> {{ $slider->phone ?? '' }} </td>
+                                                    <td> {{ $slider->subject ?? '' }} </td>
+                                                    <td> {{ $slider->date ?? '' }} </td>
+                                                    <td> {{ $slider->message ?? '' }} </td>
+                                                    <td><a href="{{ route('contactus') }}/delete/{{ $slider->id }}"
+                                                            class="btn btn-danger btn-sm"><i
+                                                                class="fas fa-trash-alt"></i></a>
+                                                    </td>
 
-                                                 
                                                 </tr>
-                                                </tr>
-                                            </thead>
-                                            <tfoot class="text-dark">
-                                                <tr>
-                                                    <th>S.NO</th>
-                                                    <th>Name </th>
-                                                    <th>Phone </th>
-                                                    <th>Email</th>
-                                                    <th>Subject</th>
-                                                    <th>Massage</th>
-                                                    <th>Delete </th>
-
-                                                 
-                                                </tr>
-                                            </tfoot>
-                                            <tbody>
-                                                @foreach ($data as $slider)
-                                                    <tr>
-                                                        <td>{{ $loop->iteration }}</td>
-
-                                                        <td> {{ $slider->name ?? '' }} </td>
-                                                        <td> {{ $slider->email ?? '' }} </td>
-                                                        <td> {{ $slider->phone ?? '' }} </td>
-                                                                  <td> {{ $slider->subject ?? '' }} </td>
-                                                        <td> {{ $slider->message ?? '' }} </td>
-
-                                                        <td><a href="{{ route('contactus') }}/delete/{{ $slider->id }}"
-                                                                class="btn btn-danger btn-sm"><i
-                                                                    class="fas fa-trash-alt"></i></a>
-                                                        </td>
-                                                   
-                                                    </tr>
-                                                @endforeach
-                                            </tbody>
-                                        </table>
-                                    </div>
+                                            @endforeach
+                                        </tbody>
+                                    </table>
                                 </div>
                             </div>
-
                         </div>
-                        <!-- /.container-fluid -->
 
                     </div>
+                    <!-- /.container-fluid -->
 
                 </div>
 
             </div>
-            <!-- Scroll to Top Button-->
+
+        </div>
+        <!-- Scroll to Top Button-->
 
 
 
-        @endslot
-    </x-layout>
+    @endslot
+</x-layout>
 
 
 
-    <!-- Page level plugins -->
-    <script src="vendor/datatables/jquery.dataTables.min.js"></script>
-    <script src="vendor/datatables/dataTables.bootstrap4.min.js"></script>
-    <script src="{{asset('ckeditor/ckeditor.js')}}"></script>
+<!-- Page level plugins -->
+<script src="vendor/datatables/jquery.dataTables.min.js"></script>
+<script src="vendor/datatables/dataTables.bootstrap4.min.js"></script>
+<script src="{{ asset('ckeditor/ckeditor.js') }}"></script>
 
-    <!-- Page level custom scripts -->
-    <script src="js/demo/datatables-demo.js"></script>
+<!-- Page level custom scripts -->
+<script src="js/demo/datatables-demo.js"></script>
